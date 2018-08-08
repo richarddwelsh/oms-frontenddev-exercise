451712fc5fca9c2c3b42e17cfc21feee8fd9c84d
Richard Welsh <richard@welshdesign.co.uk>
2018-08-08 18:51:30 +0100

    Changes

--- exercise.js
+++ exercise.js
@@ -1,41 +1,71 @@
 
 let countryData, sourceData, data = {};
 
+let path; // D3 map projection function
+let countriesGroup; // D3 selection
+
 // define some shorter identifiers for the commodity categories (used in CSS classnames, etc.)
 let categories = {
     "CerGr": {
-        label: "Cereals and Grains"
+        label: "Cereals and Grains", hue: 0,
     },
     "Pulses": {
-        label: "Pulses"
+        label: "Pulses", hue: 36,
     },
     "StaRoo": {
-        label: "Starchy Roots"
+        label: "Starchy Roots", hue: 72,
     },
     "Sugar": {
-        label: "Sugar"
+        label: "Sugar", hue: 108,
     },
     "OilFat": {
-        label: "Oils & Fats"
+        label: "Oils & Fats", hue: 144,
     },
     "Meat": {
-        label: "Meat"
+        label: "Meat", hue: 180,
     },
     "DairEgg": {
-        label: "Dairy & Eggs"
+        label: "Dairy & Eggs", hue: 216,
     },
     "FrVeg": {
-        label: "Fruit and Vegetables"
+        label: "Fruit and Vegetables", hue: 252,
     },
     "Other": {
-        label: "Other"
+        label: "Other", hue: 288,
     },
     "Alcol": {
-        label: "Alcoholic Beverages"
+        label: "Alcoholic Beverages", hue: 324
     }
-}
+};
 
 let suffix = "(FAO (2017)) (kilocalories per person per day)";
+
+// UN FAO names don't all map to names in map data
+let nameMappings = {
+    "Antigua and Barbuda" : "Antigua and Barb.",
+    "Bosnia and Herzegovina": "Bosnia and Herz.",
+    "Cabo Verde": "Cape Verde",
+    "Central African Republic": "Central African Rep.",
+    "Cote d'Ivoire": "Côte d'Ivoire",
+    "Czech Republic": "Czech Rep.",
+    "Czechoslovakia": "Czech Rep.", // ["Czech Rep.", "Slovakia"], // split in 1993
+    "Dominican Republic": "Dominican Rep.",
+    "French Polynesia": "Fr. Polynesia",
+    "Laos": "Lao PDR",
+    "North Korea": "Dem. Rep. Korea",
+    "Saint Kitts and Nevis" : "St. Kitts and Nevis",
+    "Saint Vincent and the Grenadines": "St. Vin. and Gren.",
+    "Sao Tome and Principe": "São Tomé and Principe",
+    "Serbia and Montenegro": "Serbia", // ["Serbia", "Montenegro"], // split in 2006
+    "Solomon Islands": "Solomon Is.",
+    "South Korea": "Korea",
+    "USSR": "Russia" // not an accurate mapping I know but it'll do
+};
+
+// initial seelections
+let catSelect = "Alcol";
+let yearSelect = 2000;
+
 
 // main code block will run when both source data sets have been retrieved
 Promise.all([
@@ -48,22 +78,38 @@ Promise.all([
 
         // iterate over diet composition data, creating an object with one property per country. Each country property has an array of diet composition data and geographical data
         sourceData.forEach(row => {
-            if (!(data.hasOwnProperty(row.Entity))) {
-                data[row.Entity] = {
+
+            // handle spelling differences between UN FAO and geographical data
+            let geoCountryName = nameMappings[row.Entity] || row.Entity;
+
+            if (!(data.hasOwnProperty(geoCountryName))) {
+                data[geoCountryName] = {
                     dietCompData: {},
-                    geoFeature: countryData.features.find(f => f.properties.name == row.Entity) // mapping on country name, hope this works
+                    geoFeature: countryData.features.find(f => f.properties.name == geoCountryName) // mapping on country name, hope this works
                 };
             }
 
             // each year row is transformed into an associative array using the category IDs from categories above
-            data[row.Entity].dietCompData[row.Year] = {};
+            if (!(data[geoCountryName].dietCompData[row.Year])) {
+                data[geoCountryName].dietCompData[row.Year] = {};
+            }
+
             for (var catId in categories) {
-                data[row.Entity].dietCompData[row.Year][catId] = row[categories[catId].label + " " + suffix]; 
+                data[geoCountryName].dietCompData[row.Year][catId] = Number(row[categories[catId].label + " " + suffix]); 
             }
 
             // the above can also be achieved using all sorts of new ES6 features, below (not sure if this is more or less readable!)
-            //data[row.Entity].dietCompData[row.Year] = Object.assign({}, ...Object.keys(categories).map(catId => ({[catId]: row[categories[catId].label + " " + suffix] })))
+            //data[geoCountryName].dietCompData[row.Year] = Object.assign({}, ...Object.keys(categories).map(catId => ({[catId]: row[categories[catId].label + " " + suffix] })))
         });
+
+        // calculate min/max ranges for each category
+        for (var catId in categories) {
+            let allData = sourceData.map(row => Number(row[categories[catId].label + " " + suffix]));
+
+            categories[catId].min = d3.min(allData);
+            categories[catId].max = d3.max(allData);
+        }
+
 
        // plot the map
        // lots of this is adapted from https://bl.ocks.org/andybarefoot/765c937c8599ef540e1e0b394ca89dc5 
@@ -74,13 +120,10 @@ Promise.all([
         // Using 12:5 because we will crop top and bottom of map
         w = 1000;
         h = 5/12*1000;
-        // variables for catching min and max zoom factors
-        var minZoom;
-        var maxZoom;
 
         // DEFINE FUNCTIONS/OBJECTS
         // Define map projection
-        var projection = d3
+        let projection = d3
         .geoEquirectangular()
         .center([0, 15]) // set centre to further North as we are cropping more off bottom of map
         .scale([w / (2 * Math.PI)]) // scale to fit group width
@@ -88,7 +131,7 @@ Promise.all([
         ;
 
         // Define map path
-        var path = d3
+        path = d3
             .geoPath()
             .projection(projection)
         ;
@@ -105,7 +148,7 @@ Promise.all([
         }
 
         // Define map zoom behaviour
-        var zoom = d3
+        let zoom = d3
         .zoom()
         .on("zoom", zoomed)
         ;
@@ -115,7 +158,7 @@ Promise.all([
             .attr("height", h)
             .call(zoom);
 
-        let countriesGroup = svg.append("g").attr("id", "map");
+        countriesGroup = svg.append("g").attr("id", "map");
         // add a background rectangle
         countriesGroup
             .append("rect")
@@ -124,7 +167,10 @@ Promise.all([
             .attr("width", w)
             .attr("height", h);
 
+        drawMap(yearSelect, catSelect);
+    })
 
+function drawMap(year, category) {
         // DATA JOIN
         let countries = countriesGroup
             .selectAll("path")
@@ -134,9 +180,35 @@ Promise.all([
         let countriesEnter = countries
             .enter()
             .append("path")
-            // .attr("d", d => path(d.geoFeature))
+            .attr("d", d => path(d.geoFeature))
             .attr("id", function(d, i) {
               return "country" + d.geoFeature.properties.iso_a3;
             })
-            .attr("class", "country");
-    })
+            .attr("class", "country")
+            .on("click", countryClickHandler)
+            .on("mouseover", countryMouseoverHandler)
+            .on("mouseout", countryMouseoutHandler);
+
+        // ENTER + UPDATE (what to do when creating OR updating data elements) - update => change fill colour of country
+        countries.merge(countriesEnter)
+            .attr("fill", d => {
+                if (d.dietCompData[yearSelect] && d.dietCompData[yearSelect][catSelect]) {
+                    let pc = d.dietCompData[yearSelect][catSelect] * 100 / categories[catSelect].max ;
+                    return `hsl(${categories[catSelect].hue},75%,${Math.floor(100 - pc)}%)`;
+                }
+                else
+                    return "hsl(0, 0%, 70%)"
+            })
+}
+
+function countryClickHandler(d, i) {
+    //console.log(d.properties.name)
+}
+
+function countryMouseoverHandler(d, i) {
+    //console.log(d.properties.name)
+}
+
+function countryMouseoutHandler(d, i) {
+    //console.log(d.properties.name)
+}
